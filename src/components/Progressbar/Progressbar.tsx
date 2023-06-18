97f86be67b6629a5f39e5e1217ed690468f0cd4c
Mohammad <id.mmehrabi@gmail.com>
2023-06-18 03:59:39 +0330

    change build package from rollup to tsup

--- src/components/Progressbar/Progressbar.tsx
+++ src/components/Progressbar/Progressbar.tsx
@@ -1,3 +1,4 @@
+import * as React from "react";
 import {useNavigation} from "@remix-run/react";
 
 const Progressbar = () => {
---
+++ tsup.config.ts
@@ -0,0 +1,12 @@
+import {Options} from "tsup";
+
+export const tsup: Options = {
+    target: 'esnext',
+    clean: true,
+    dts: true,
+    entry: ['src/index.ts'],
+    keepNames: true,
+    minify: true,
+    sourcemap: true,
+    format: ['cjs'],
+}
